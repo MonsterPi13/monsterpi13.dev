cc80ebae6fec07077feada2e8c7b846687538942
MonsterPi13 <rpishappynow@gmail.com>
2024-01-19 19:17:16 +0800

    fix: contentful webhooks

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
 
 export async function POST(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
@@ -9,9 +9,7 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
   }
 
-  const searchParams = request.nextUrl.searchParams;
-  const path = searchParams.get("path") || "/";
-  revalidatePath(path);
+  revalidateTag("articles");
 
   return NextResponse.json(
     { revalidated: true, now: Date.now() },
--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -16,6 +16,7 @@ async function fetchGraphQL(query: string, preview = isDevelopment) {
         }`,
       },
       body: JSON.stringify({ query }),
+      next: { tags: ["articles"] },
     }
   );
   if (!res.ok) return undefined;
