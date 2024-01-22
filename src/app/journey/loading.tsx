9830ced6bd6e57e024e766e87beb5a4f4bb72a1f
MonsterPi13 <rpishappynow@gmail.com>
2024-01-22 14:57:07 +0800

    feat: add loading placeholder in journey page

---
+++ src/app/journey/loading.tsx
@@ -0,0 +1,5 @@
+import LoadingSpinner from "@/components/loading-spinner";
+
+export default function Loading() {
+  return <LoadingSpinner />;
+}
---
+++ src/components/loading-spinner.tsx
@@ -0,0 +1,15 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="grid place-content-center h-screen w-full">
+      <div
+        className="inline-block h-4 w-4 animate-spin rounded-full border-[2px] border-current border-t-transparent text-black"
+        role="status"
+        aria-label="loading"
+      >
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
