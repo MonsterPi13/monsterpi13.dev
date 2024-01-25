fcf8d072e7d2f0003995096ce37b9576c1b7cc48
MonsterPi13 <rpishappynow@gmail.com>
2024-01-25 16:26:29 +0800

    feat: add subscribe logic

--- src/hooks/useViewData.ts
+++ src/hooks/useViewData.ts
@@ -1,65 +1,60 @@
-"use client";
+'use client'
 
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
-import supabase from "@/lib/supabase/public";
-import { SUPABASE_TABLE_NAME } from "@/lib/constants";
+import supabase from '@/lib/supabase/public'
+import { SUPABASE_TABLE_NAME } from '@/lib/constants'
 
-import type { ViewItem } from "@/types/contentful";
+import type { ViewItem } from '@/types/contentful'
 
 export const useViewData = (slug?: string) => {
-  const [viewData, setViewData] = useState<ViewItem[] | null>(null);
+  const [viewData, setViewData] = useState<ViewItem[] | null>(null)
 
   useEffect(() => {
     async function getViewData() {
       try {
-        const supabaseQuery = supabase
-          .from(SUPABASE_TABLE_NAME)
-          .select("slug, view_count");
-        if (slug) supabaseQuery.eq("slug", slug);
-        const { data: supabaseData } = await supabaseQuery;
-        if (supabaseData) setViewData(supabaseData as unknown as ViewItem[]);
+        const supabaseQuery = supabase.from(SUPABASE_TABLE_NAME).select('slug, view_count')
+        if (slug) supabaseQuery.eq('slug', slug)
+        const { data: supabaseData } = await supabaseQuery
+        if (supabaseData) setViewData(supabaseData as unknown as ViewItem[])
       } catch (error) {
-        console.info("Error fetching view data from Supabase:", error);
+        console.info('Error fetching view data from Supabase:', error)
       }
     }
 
-    getViewData();
-  }, [slug]);
+    getViewData()
+  }, [slug])
 
-  // useEffect(() => {
-  //   function handleRealtimeChange(payload) {
-  //     if (payload?.new?.slug) {
-  //       setViewData((prev) => {
-  //         if (!prev) return null;
-  //         const index = prev.findIndex(
-  //           (item) => item.slug === payload.new.slug
-  //         );
-  //         if (index)
-  //           index !== -1 ? (prev[index] = payload.new) : prev.push(payload.new);
-  //         return [...prev];
-  //       });
-  //     }
-  //   }
+  useEffect(() => {
+    function handleRealtimeChange(payload: any) {
+      if (payload?.new?.slug) {
+        setViewData((prev) => {
+          if (!prev) return null
+          const index = prev.findIndex((item) => item.slug === payload.new.slug)
+          index !== -1 ? (prev[index] = payload.new) : prev.push(payload.new)
+          return [...prev]
+        })
+      }
+    }
 
-  //   const channel = supabase
-  //     .channel("supabase_realtime")
-  //     .on(
-  //       "postgres_changes",
-  //       {
-  //         event: "UPDATE",
-  //         schema: "public",
-  //         table: SUPABASE_TABLE_NAME,
-  //         ...(slug && { filter: `slug=eq.${slug}` }),
-  //       },
-  //       handleRealtimeChange
-  //     )
-  //     .subscribe();
+    const channel = supabase
+      .channel('supabase_realtime')
+      .on(
+        'postgres_changes',
+        {
+          event: 'UPDATE',
+          schema: 'public',
+          table: SUPABASE_TABLE_NAME,
+          ...(slug && { filter: `slug=eq.${slug}` })
+        },
+        handleRealtimeChange
+      )
+      .subscribe()
 
-  //   return () => {
-  //     supabase.removeChannel(channel);
-  //   };
-  // }, [slug]);
+    return () => {
+      supabase.removeChannel(channel)
+    }
+  }, [slug])
 
-  return viewData;
-};
+  return viewData
+}
